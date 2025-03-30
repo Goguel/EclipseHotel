4adba083f0ff30abecba2f9185cfec19a0fb4c84
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-30 20:54:34 -0300

    feat: Add attributes to Rooms model

--- src/main/java/com/miguelx/eclipseHotel/model/Rooms.java
+++ src/main/java/com/miguelx/eclipseHotel/model/Rooms.java
@@ -1,4 +1,35 @@
 package com.miguelx.eclipseHotel.model;
 
+import com.miguelx.eclipseHotel.dto.RoomsRequestDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity(name = "rooms")
+@Table(name = "rooms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Rooms {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer room_number;
+    private String type;
+    private Float price;
+
+    @OneToMany(mappedBy = "rooms_id")
+    private List<Reservations> reservations;
+
+    public Rooms(RoomsRequestDTO data) {
+        this.room_number = data.getRoom_number();
+        this.type = data.getType();
+        this.price = data.getPrice();
+    }
+
+    public Rooms(Long roomsId) {
+    }
 }
