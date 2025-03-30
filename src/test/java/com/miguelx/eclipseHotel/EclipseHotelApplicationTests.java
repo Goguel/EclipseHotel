a5d6b79d5e03edb1da5d84ccc86d7369b562f8fd
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-29 23:28:57 -0300

    feat: Download project configs from Spring Initializr

---
+++ src/test/java/com/miguelx/eclipseHotel/EclipseHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguelx.eclipseHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
