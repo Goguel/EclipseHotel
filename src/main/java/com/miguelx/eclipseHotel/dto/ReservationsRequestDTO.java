9c45438187d1add6df9cbc015af821acade0a583
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-30 20:42:33 -0300

    feat: Add attributes to Reservations DTOs

--- src/main/java/com/miguelx/eclipseHotel/dto/ReservationsRequestDTO.java
+++ src/main/java/com/miguelx/eclipseHotel/dto/ReservationsRequestDTO.java
@@ -1,4 +1,16 @@
 package com.miguelx.eclipseHotel.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class ReservationsRequestDTO {
+
+    private Long customers_id;
+    private Long rooms_id;
+    private LocalDate checkin;
+    private LocalDate checkout;
 }
--- src/main/java/com/miguelx/eclipseHotel/dto/ReservationsResponseDTO.java
+++ src/main/java/com/miguelx/eclipseHotel/dto/ReservationsResponseDTO.java
@@ -1,4 +1,18 @@
 package com.miguelx.eclipseHotel.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class ReservationsResponseDTO {
+
+    private Long id;
+    private RoomsResponseDTO rooms;
+    private CustomersResponseDTO customers;
+    private LocalDate checkin;
+    private LocalDate checkout;
+
 }
