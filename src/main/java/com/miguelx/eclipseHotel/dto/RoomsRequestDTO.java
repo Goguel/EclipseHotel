4f7e1b39b414d9d577a4af85749b91a4ad6f5700
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-30 20:44:07 -0300

    feat: Add attributes to Rooms DTOs

--- src/main/java/com/miguelx/eclipseHotel/dto/RoomsRequestDTO.java
+++ src/main/java/com/miguelx/eclipseHotel/dto/RoomsRequestDTO.java
@@ -1,4 +1,14 @@
 package com.miguelx.eclipseHotel.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class RoomsRequestDTO {
+
+    private Integer room_number;
+    private String type;
+    private Float price;
+
 }
--- src/main/java/com/miguelx/eclipseHotel/dto/RoomsResponseDTO.java
+++ src/main/java/com/miguelx/eclipseHotel/dto/RoomsResponseDTO.java
@@ -1,4 +1,19 @@
 package com.miguelx.eclipseHotel.dto;
 
+import com.miguelx.eclipseHotel.model.Rooms;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class RoomsResponseDTO {
+    private Long id;
+    private Integer room_number;
+    private String type;
+    private Float price;
+
+    public RoomsResponseDTO(Rooms roomsId) {
+    }
 }
