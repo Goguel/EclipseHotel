0f7fb48c6f7d553c69776ed7c3454d9677298eca
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-30 20:52:13 -0300

    feat: Add attributes to Customers model

--- src/main/java/com/miguelx/eclipseHotel/model/Customers.java
+++ src/main/java/com/miguelx/eclipseHotel/model/Customers.java
@@ -1,4 +1,32 @@
 package com.miguelx.eclipseHotel.model;
 
+import com.miguelx.eclipseHotel.dto.CustomersRequestDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity(name = "customers")
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Customers {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+
+    // @OneToMany(mappedBy = "customers_id")
+    //  private List<Reservations> reservations;
+
+    public Customers(CustomersRequestDTO data) {
+        this.name = data.getName();
+        this.email = data.getEmail();
+        this.phone = data.getPhone();
+    }
 }
