ad0d58759a7cef4323e7366969d99e19c75b0aa9
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-30 20:38:48 -0300

    feat: Add attributes to Customers DTOs

--- src/main/java/com/miguelx/eclipseHotel/dto/CustomersRequestDTO.java
+++ src/main/java/com/miguelx/eclipseHotel/dto/CustomersRequestDTO.java
@@ -1,4 +1,13 @@
 package com.miguelx.eclipseHotel.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CustomersRequestDTO {
+
+    private String name;
+    private String email;
+    private String phone;
 }
--- src/main/java/com/miguelx/eclipseHotel/dto/CustomersResponseDTO.java
+++ src/main/java/com/miguelx/eclipseHotel/dto/CustomersResponseDTO.java
@@ -1,4 +1,19 @@
 package com.miguelx.eclipseHotel.dto;
 
+import com.miguelx.eclipseHotel.model.Customers;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class CustomersResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+
+    public CustomersResponseDTO(Customers customersId) {
+    }
 }
