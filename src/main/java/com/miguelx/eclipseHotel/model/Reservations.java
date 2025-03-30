d26498d4a58c32a68121636b8fba31b2c372fbfc
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-30 20:54:23 -0300

    feat: Add attributes to Reservations model

--- src/main/java/com/miguelx/eclipseHotel/model/Reservations.java
+++ src/main/java/com/miguelx/eclipseHotel/model/Reservations.java
@@ -1,4 +1,35 @@
 package com.miguelx.eclipseHotel.model;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "reservations")
+@Table(name = "reservations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Reservations {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "customers_id")
+    private Customers customers_id;
+    @NonNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "rooms_id")
+    private Rooms rooms_id;
+
+    @JsonUnwrapped
+    private Status status;
+
+    private LocalDate checkin;
+    private LocalDate checkout;
+
 }
