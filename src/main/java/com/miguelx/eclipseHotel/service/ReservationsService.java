03fad1dc2d0de282df01c1747c42df8bb68f4610
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-30 21:45:03 -0300

    feat: Add CRUD methods to ReservationsService

--- src/main/java/com/miguelx/eclipseHotel/service/ReservationsService.java
+++ src/main/java/com/miguelx/eclipseHotel/service/ReservationsService.java
@@ -1,4 +1,27 @@
 package com.miguelx.eclipseHotel.service;
 
+import com.miguelx.eclipseHotel.model.Reservations;
+import com.miguelx.eclipseHotel.repository.ReservationsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReservationsService {
+
+    @Autowired
+    private ReservationsRepository reservationsRepository;
+
+    public List<Reservations> listaTodos() {
+        return (List<Reservations>) reservationsRepository.findAll();
+    }
+
+    public Reservations salvar(Reservations reservations) {
+        return reservationsRepository.save(reservations);
+    }
+
+    public void deletar(Long id) {
+        reservationsRepository.deleteById(id);
+    }
 }
