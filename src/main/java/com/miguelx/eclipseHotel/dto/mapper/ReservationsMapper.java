c1446afc975b951a995f2107b1f05acd5676c5aa
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-03-31 09:46:57 -0300

    feat: Add conversion methods to ReservationsMapper

--- src/main/java/com/miguelx/eclipseHotel/dto/mapper/ReservationsMapper.java
+++ src/main/java/com/miguelx/eclipseHotel/dto/mapper/ReservationsMapper.java
@@ -1,4 +1,32 @@
 package com.miguelx.eclipseHotel.dto.mapper;
 
+import com.miguelx.eclipseHotel.dto.ReservationsRequestDTO;
+import com.miguelx.eclipseHotel.dto.ReservationsResponseDTO;
+import com.miguelx.eclipseHotel.model.Reservations;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class ReservationsMapper {
+
+    @Autowired
+    public ModelMapper reservationsMapper;
+
+    public Reservations toEntity(ReservationsRequestDTO dto) {
+        return  reservationsMapper.map(dto, Reservations.class);
+    }
+
+    public ReservationsResponseDTO toDTO(Reservations entity) {
+        return reservationsMapper.map(entity, ReservationsResponseDTO.class);
+    }
+
+    public List<ReservationsResponseDTO> toDTOList(List<Reservations> reservations) {
+        return reservations.stream()
+                .map(this::toDTO)
+                .collect(Collectors.toList());
+    }
 }
